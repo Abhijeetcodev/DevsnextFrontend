fcb3b2b45e4e1a22423d65a630fa7b9232e02dde
Abhijeet singh <58856747+Abhijeetcodev@users.noreply.github.com>
2021-06-17 15:59:08 +0530

    Add files via upload

---
+++ Day5&6/Q5.js
@@ -0,0 +1,7 @@
+
+var arr = ["Red","Green","White","Black"]; 
+    
+    console.log(' "'+arr.join(",")+'" ');
+    console.log(' "'+arr.join(",")+'" ');
+    console.log(' "'+arr.join("+")+'" ');
+
